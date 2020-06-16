ab82d5f1c89ccc3cf56931d788b46cc36bcfadc7
Carl <carlmeyer1996@gmail.com>
2020-06-16 22:28:54 +0200

    Change actions to recyclerview

--- QuestGeneratorDemo/app/src/main/java/com/carlmeyer/questgeneratordemo/ui/fragments/QuestGeneratorFragment.java
+++ QuestGeneratorDemo/app/src/main/java/com/carlmeyer/questgeneratordemo/ui/fragments/QuestGeneratorFragment.java
@@ -2,6 +2,7 @@ package com.carlmeyer.questgeneratordemo.ui.fragments;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,24 +11,37 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.carlmeyer.questgeneratordemo.R;
 import com.carlmeyer.questgeneratordemo.questgenerator.data.Motives;
 import com.carlmeyer.questgeneratordemo.questgenerator.generator.QuestGenerator;
 import com.carlmeyer.questgeneratordemo.questgenerator.generator.QuestReader;
 import com.carlmeyer.questgeneratordemo.questgenerator.models.Action;
+import com.carlmeyer.questgeneratordemo.questgenerator.models.Location;
 import com.carlmeyer.questgeneratordemo.questgenerator.models.Quest;
+import com.carlmeyer.questgeneratordemo.ui.adapters.ActionsAdapter;
+import com.carlmeyer.questgeneratordemo.ui.adapters.LocationsAdapter;
+import com.carlmeyer.questgeneratordemo.ui.viewholders.ActionViewHolder;
+import com.yarolegovich.lovelydialog.LovelyStandardDialog;
 
 import java.util.List;
 import java.util.Random;
 
-public class QuestGeneratorFragment extends Fragment {
+import io.realm.OrderedRealmCollection;
+
+public class  QuestGeneratorFragment extends Fragment implements ActionViewHolder.OnActionListener {
 
     private String[] motivations = new String[]{Motives.KNOWLEDGE, Motives.COMFORT, Motives.JUSTICE};
 
     private TextView tvMotivation;
     private TextView tvDescription;
     private TextView tvQuest;
+
+    private ActionsAdapter actionsAdapter;
+    private RecyclerView rvActions;
+    List<Action> questSteps;
 
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -39,6 +53,8 @@ public class QuestGeneratorFragment extends Fragment {
         tvDescription = root.findViewById(R.id.tvDescription);
         tvQuest = root.findViewById(R.id.tvQuest);
         Button btnGenerateQuest = root.findViewById(R.id.btnQuestGenerator);
+
+        rvActions = root.findViewById(R.id.rvActions);
 
         btnGenerateQuest.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -83,7 +99,9 @@ public class QuestGeneratorFragment extends Fragment {
             String questDescriptionText = questReader.getQuestDescriptionText();
             String questMotivationText = questReader.getQuestMotivationText();
             String questStepsText = questReader.getQuestStepsToString();
-            List<Action> questSteps = questReader.getQuestSteps();
+            questSteps = questReader.getQuestSteps();
+
+            setUpRecyclerView();
 
             tvMotivation.setText("Motivation : " + questMotivationText);
             tvDescription.setText("Description : " + questDescriptionText);
@@ -92,5 +110,22 @@ public class QuestGeneratorFragment extends Fragment {
         } else {
             tvQuest.setText("Error! Quest is null!");
         }
+    }
+
+    /**
+     * Set up RecyclerView
+     */
+    private void setUpRecyclerView() {
+        // Create and set layoutManager
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getContext());
+        rvActions.setLayoutManager(layoutManager);
+        // Initialize and set locationsAdapter with list of locations
+        actionsAdapter = new ActionsAdapter(questSteps, this);
+        rvActions.setAdapter(actionsAdapter);
+    }
+
+    @Override
+    public void onActionClick(int position) {
+
     }
 }
